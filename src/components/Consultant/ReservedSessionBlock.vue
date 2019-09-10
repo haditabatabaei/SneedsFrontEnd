4223a4f61e461cabf4a9e5163948851a226727bc
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2019-09-10 23:55:22 +0430

    user reserved sessions styles updated

---
+++ src/components/Consultant/ReservedSessionBlock.vue
@@ -0,0 +1,226 @@
+<template>
+    <div class="col-md-3 reservedCard text-center">
+        <img class="reservedCard--image" :src="session.consultant.profile_picture"
+             :alt="session.consultant.first_name +' '+ session.consultant.last_name" v-if="!isConsultant">
+        <h5 class="reservedCard--detail-name isansFont--faNum text-center noImageTitle" v-if="isConsultant"
+            :class="[{'noImageTitle' : isConsultant}]">
+            توسط :
+            {{session.sold_to.first_name + " " + session.sold_to.last_name}}
+        </h5>
+        <h5 class="reservedCard--detail-name isansFont--faNum text-center" v-else>
+            {{session.consultant.first_name + " " + session.consultant.last_name}}
+        </h5>
+        <p class="reservedCard--detail-remain isansFont--faNum" :class="[{'deactiveRemain': currentTimeAfterSession}]">
+            {{getJalali(session.start_time).locale('fa').fromNow()}}
+        </p>
+        <p class="reservedCard--detail-date isansFont--faNum">
+            <time>
+                {{getJalali(session.start_time).locale('fa').format('dddd YYYY/MM/DD')}}
+            </time>
+            <br>
+            <span>از ساعت</span>
+            <time :datetime="getJalali(session.start_time)">
+                {{getJalali(session.start_time).locale('fa').format('HH:mm')}}
+            </time>
+            <span>تا</span>
+            <time :datetime="getJalali(session.end_time)">
+                {{getJalali(session.end_time).locale('fa').format('HH:mm')}}
+            </time>
+        </p>
+
+        <div class="reservedCard--actions">
+            <a :href="session.login_url" class="isansFont--faNum reservedCard--enterButton btn btn-success" v-if="currentTimeBetweenSession">ورود به جلسه</a>
+            <button disabled class="isansFont--faNum reservedCard--enterButton btn btn-default" v-else-if="currentTimeBeforeSession">ورود به جلسه
+            </button>
+        </div>
+
+        <div class="isansFont--faNum reservedCard--rate text-center" v-if="currentTimeAfterSession">
+            <div class="col-md-12" v-if="canRate && !isConsultant">
+                <StarRating v-model="session.starRate"
+                            :rtl="true"
+                            :star-size="25"
+                            :rounded-corners="true"
+                ></StarRating>
+                <button @click="submitRate(session.id,session.starRate)"
+                        class="isansFont btn btn-rose btn-sm btn-round">ثبت امتیاز
+                </button>
+            </div>
+            <p v-else-if="canRate && isConsultant">
+                کاربر امتیاز خود را ثبت کرده است
+            </p>
+            <p v-else-if="!canRate && isConsultant">
+                کاربر امتیاز خود را ثبت نکرده است
+            </p>
+            <p v-else>
+                شما امتیاز خود را ثبت کردید :
+                {{rate}}
+            </p>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    import jalali from 'jalali-moment'
+    import axios from 'axios'
+    import StarRating from 'vue-star-rating'
+
+    export default {
+        name: "ReservedSessionBlock",
+        components: {StarRating},
+        props: {
+            session: {},
+            isConsultant: {},
+            rate: {},
+        },
+        data: function () {
+            return {}
+        },
+        methods: {
+            getJalali: function (date) {
+                return jalali(date);
+            },
+
+            sendConsultantRate: function (soldSlotId, rate) {
+                return new Promise((resolve, reject) => {
+                    axios({
+                        url: this.$store.getters.getApi + 'comment/sold-time-slot-rates/',
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': 'JWT ' + this.$store.getters.getToken,
+                        },
+                        data: {
+                            "sold_time_slot": soldSlotId,
+                            "rate": rate,
+                        }
+                    }).then(response => {
+                        resolve(response);
+                    }).catch(error => {
+                        reject(error);
+                    })
+                })
+            },
+
+            submitRate: function (soldSlotId, rate) {
+                console.log('submitPressed');
+                this.sendConsultantRate(soldSlotId, rate).then(response => {
+                    console.log('response is here emitting update list event', response);
+                    this.$emit('update-list');
+                }).catch(error => {
+                    console.log(error);
+                    if (error.response)
+                        console.log(error.response);
+                })
+            },
+        },
+        computed: {
+            currentTimeBetweenSession: function () {
+                return this.getJalali().isBetween(this.getJalali(this.session.start_time), this.getJalali(this.session.end_time));
+            },
+
+            canRate: function () {
+                return this.rate === false;
+            },
+
+            currentTimeBeforeSession: function () {
+                return this.getJalali().isBefore(this.getJalali(this.session.start_time));
+            },
+            currentTimeAfterSession: function () {
+                return this.getJalali().isAfter(this.getJalali(this.session.end_time));
+            },
+
+            nearTime: function () {
+
+            },
+
+            mediumTime: function () {
+
+            },
+            farTime: function () {
+
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .reservedCard {
+        background-color: white;
+        border-radius: 5px;
+        border: 1px solid #969696;
+
+        margin-top: 30px;
+
+        width: calc(25% - 30px);
+
+        margin-right: 15px;
+        margin-left: 15px;
+
+        height: 320px;
+    }
+
+    .reservedCard--image {
+        border-radius: 20px;
+        height: 110px;
+        width: 110px;
+
+        position: relative;
+        top: -20px;
+
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+    }
+
+    .reservedCard--detail-name {
+        margin-top: -10px;
+        margin-bottom: 15px;
+        font-weight: bold;
+    }
+
+    .reservedCard--detail-name.noImageTitle {
+        margin-top: 15px;
+    }
+
+    .reservedCard--detail-remain {
+        border-radius: 20px;
+        font-weight: bold;
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+
+    .deactiveRemain {
+        background-color: #f1f1f1;
+        color: #bebebe;
+    }
+
+    .reservedCard--detail-date time {
+        font-weight: bold;
+    }
+
+    .reservedCard--enterButton {
+        border-radius: 10px;
+        font-weight: bold;
+        padding: 8px 20px;
+        margin-top: 0;
+    }
+
+    .reservedCard--enterButton[disabled] {
+        background-color: #f1f1f1;
+        opacity: 1;
+        color: #bebebe;
+    }
+
+    .reservedCard--rate .vue-star-rating {
+        justify-content: center;
+    }
+
+
+    @media only screen and (max-width: 991.8px) {
+        .reservedCard {
+            width: 100%;
+            margin-right: 0;
+            margin-left: 0;
+        }
+    }
+</style>
--- src/views/UserReservedSessions.vue
+++ src/views/UserReservedSessions.vue
@@ -40,94 +40,15 @@
                                 مشاهده مشاوران
                             </router-link>
                         </div>
-                        <div class="card card-profile card-plain col-md-6 flexCard" v-for="session in reservedSessions">
-                            <div class="card-image">
-                                <router-link :to="'consultants/' + session.consultant.slug">
-                                    <img class="img img-fluid"
-                                         v-bind:src="session.consultant.profile_picture"
-                                         alt="image">
-                                </router-link>
-                            </div>
-                            <div class="col-md-7">
-                                <div class="card-content">
-                                    <h4 class="card-title isansFont"
-                                        v-if="!isConsultant">
-                                        مشاور :
-                                        {{session.consultant.first_name + " " + session.consultant.last_name }}
-                                    </h4>
-                                    <h4 class="card-title isansFont" v-else>
-                                        کاربر :
-                                        {{session.sold_to.first_name + ' ' + session.sold_to.last_name}}
-                                    </h4>
-                                    <br>
-                                    <p class="isansFont">
-                                        تاریخ برگزاری :
-                                        {{getJalali(session.start_time).locale('fa').format('YYYY/MM/DD dddd')}}
-                                        <br>
-                                        ساعت :
-                                        <br>
-                                        {{getJalali(session.start_time).locale('fa').format('HH:mm:ss')}}
-                                        تا
-                                        {{getJalali(session.end_time).locale('fa').format('HH:mm:ss')}}
-                                    </p>
 
-                                    <p v-if="session.used" class="text-success isansFont">
-                                        وضعیت جلسه :
-                                        برگزار شده
-                                    </p>
-                                    <p v-else-if="getJalali(session.start_time).isBefore(jalaliNow)"
-                                       class="text-danger isansFont">
-                                        وضعیت جلسه :
-                                        برگزار نشده - منقضی شده
-                                    </p>
-                                    <p v-else class="text-danger isansFont">برگزار نشده</p>
+                        <ReservedSessionBlock v-for="session in reservedSessions"
+                                              :isConsultant="isConsultant"
+                                              :session="session"
+                                              :rate="slotHasRate(session.id)"
+                                              @update-list="updateList()"
+                                              :key="session.id"
+                        ></ReservedSessionBlock>
 
-                                    <p class="text-success isansFont"
-                                       v-if="jalaliNow.isBetween(getJalali(session.start_time),getJalali(session.end_time))">
-                                        در حال برگزاری
-                                    </p>
-
-                                    <div class="cart-flex-footer mt-10">
-                                        <a :disabled="!session.login_url" class="isansFont btn btn-success btn-sm"
-                                           target="_blank" :href="session.login_url">
-                                            ورود به جلسه
-                                        </a>
-                                        <router-link v-if="!isConsultant" class="btn btn-info isansFont btn-sm"
-                                                     :to="'/consultants/' + session.consultant.slug">صفحه مشاور
-                                        </router-link>
-                                    </div>
-                                    <p class="text-info isansFont">
-                                        {{getJalali(session.start_time).locale('fa').fromNow()}}
-                                    </p>
-                                    <div class="rateWrapper" v-if="!isConsultant && !slotHasRate(session.id)">
-                                        <StarRating v-model="session.starRate"
-                                                    :rtl="true"
-                                                    :star-size="25"
-                                                    :rounded-corners="true">
-                                        </StarRating>
-                                        <button @click="submitRate(session.id,session.starRate)"
-                                                class="isansFont btn btn-rose btn-sm btn-round">ثبت امتیاز
-                                        </button>
-                                    </div>
-                                    <div v-else-if="!isConsultant && slotHasRate(session.id)">
-                                        <p class="isansFont">
-                                            شما به مشاور در این جلسه امتیاز {{slotHasRate(session.id)}}
-                                            از 5 دادید
-                                        </p>
-                                    </div>
-                                    <div v-else-if="isConsultant && slotHasRate(session.id)">
-                                        <p class="isansFont">
-                                            این کاربر امتیاز خود را ثبت کرده است.
-                                        </p>
-                                    </div>
-                                    <div v-else-if="isConsultant && !slotHasRate(session.id)">
-                                        <p class="isansFont">
-                                            این کاربر هنوز به شما در این جلسه امتیازی نداده است.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -139,11 +60,12 @@
     import axios from 'axios'
     import jalali from 'jalali-moment'
     import RectNotifBlock from '@/components/NotifBlocks/RectNotifBlock'
+    import ReservedSessionBlock from '@/components/Consultant/ReservedSessionBlock'
     import StarRating from 'vue-star-rating'
 
     export default {
         name: "UserReservedSessions",
-        components: {RectNotifBlock, StarRating},
+        components: {RectNotifBlock, ReservedSessionBlock, StarRating},
         data() {
             return {
                 roomInterval: null,
@@ -182,33 +104,7 @@
             }
         },
         created() {
-            this.resetCartsLogic();
-            this.startCartsLogic();
-            this.getReservedTimes().then((responseSessions) => {
-                this.getListOfRates().then(responseRates => {
-                    for (let i = 0; i < responseSessions.data.length; i++) {
-                        responseSessions.data[i]["starRate"] = null;
-                        responseSessions.data[i]["login_url"] = null;
-                    }
-                    this.reservedSessions = responseSessions.data;
-                    this.availableRates = responseRates.data;
-                    console.log('reserved sessions:', this.reservedSessions);
-                    console.log('rates :', this.availableRates);
-
-                    console.log('done');
-                    this.getVideoRooms().then(() => {
-                        console.log(this.reservedSessions);
-                        this.resetCartsLogic();
-
-                    }).catch(() => {
-                        this.failedCartsLogic();
-                    });
-                }).catch(error => {
-                    this.failedCartsLogic();
-                });
-            }).catch(() => {
-                this.failedCartsLogic();
-            });
+            this.updateList();
         },
         mounted() {
             scrollTo(0, 0);
@@ -224,9 +120,38 @@
         },
         destroyed() {
             clearInterval(this.roomInterval);
-            clearTimeout();
         },
         methods: {
+
+            updateList: function(){
+                this.resetCartsLogic();
+                this.startCartsLogic();
+                this.getReservedTimes().then((responseSessions) => {
+                    this.getListOfRates().then(responseRates => {
+                        for (let i = 0; i < responseSessions.data.length; i++) {
+                            responseSessions.data[i]["starRate"] = null;
+                            responseSessions.data[i]["login_url"] = null;
+                        }
+                        this.reservedSessions = responseSessions.data;
+                        this.availableRates = responseRates.data;
+                        console.log('reserved sessions:', this.reservedSessions);
+                        console.log('rates :', this.availableRates);
+
+                        console.log('done');
+                        this.getVideoRooms().then(() => {
+                            console.log(this.reservedSessions);
+                            this.resetCartsLogic();
+
+                        }).catch(() => {
+                            this.failedCartsLogic();
+                        });
+                    }).catch(error => {
+                        this.failedCartsLogic();
+                    });
+                }).catch(() => {
+                    this.failedCartsLogic();
+                });
+            },
 
             showStarRate: function () {
                 console.log('star rate :', this.starRate)
@@ -352,59 +277,6 @@
                 return null;
             },
 
-            submitRate: function (soldSlotId, rate) {
-                return new Promise((resolve, reject) => {
-                    this.sendConsultantRate(soldSlotId, rate).then(response => {
-                        console.log(response);
-                        this.resetCartsLogic();
-                        this.startCartsLogic();
-                        this.getReservedTimes().then((responseSessions) => {
-                            this.getListOfRates().then(responseRates => {
-                                for (let i = 0; i < responseSessions.data.length; i++) {
-                                    responseSessions.data[i]["starRate"] = null;
-                                }
-                                this.reservedSessions = responseSessions.data;
-                                this.availableRates = responseRates.data;
-                                console.log('reserved sessions:', this.reservedSessions);
-                                console.log('rates :', this.availableRates);
-                                this.resetCartsLogic();
-                            }).catch(error => {
-                                this.failedCartsLogic();
-                            });
-                        }).catch(() => {
-                            this.failedCartsLogic();
-                        });
-                        resolve();
-                    }).catch(error => {
-                        console.log(error);
-                        if (error.response)
-                            console.log(error.response);
-                        reject();
-                    })
-                })
-            },
-
-            sendConsultantRate: function (soldSlotId, rate) {
-                return new Promise((resolve, reject) => {
-                    axios({
-                        url: this.$store.getters.getApi + 'comment/sold-time-slot-rates/',
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': 'JWT ' + this.$store.getters.getToken,
-                        },
-                        data: {
-                            "sold_time_slot": soldSlotId,
-                            "rate": rate,
-                        }
-                    }).then(response => {
-                        resolve(response);
-                    }).catch(error => {
-                        reject(error);
-                    })
-                })
-            },
-
             getListOfRates: function () {
                 return new Promise((resolve, reject) => {
                     axios({
@@ -461,40 +333,8 @@
 
 <style scoped>
 
-    .rateWrapper {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .flexCard {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        flex-wrap: wrap;
-        justify-content: space-evenly;
-    }
-
-    .flexCard img {
-        max-width: 150px;
-    }
-
-    .flexCard .card-image {
-        margin-top: 10px;
-        margin-right: 10px;
-        margin-bottom: 10px;
-    }
-
-    .flexCard:hover {
-        background-color: #eeeeee;
-    }
-
-    .cart-flex-footer {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
+    .main {
+        background-color: #f1f1f1;
+        padding-bottom:30px;
     }
 </style>

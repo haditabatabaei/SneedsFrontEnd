aa6fe424a71ab7e1259f459f11e3e3bf894873b7
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-03-14 00:03:33 +0330

    Config files updated.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
-module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+const removeConsolePlugin = [];
+if (process.env.NODE_ENV === 'production') {
+    removeConsolePlugin.push("transform-remove-console")
 }
+module.exports = {
+    presets: [
+        '@vue/app'
+    ],
+    plugins : removeConsolePlugin
+};

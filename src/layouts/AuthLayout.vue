ce422b3184f683a03ce966ec167366bbb31a3fde
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-03-14 18:57:00 +0330

    Layout for auth pages added.

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="authWrapper">
+        <div class="authFormWrapper">
+            <div class="authFormWrapper-switcher isansFont">
+                <router-link to="/auth/login" class="switcher" :class="[{'switcher--active' : $route.name === 'auth-login'}]">ورود</router-link>
+                <router-link to="/auth/register" class="switcher" :class="[{'switcher--active' : $route.name === 'auth-register'}]">ثبت نام</router-link>
+            </div>
+            <router-view :key="$route.fullPath" />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AuthLayout",
+    }
+</script>
+
+<style scoped>
+    .authWrapper {
+        width: 100%;
+        min-height: calc(100vh - 70px);
+        margin-top: 70px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        /*background-color: white;*/
+    }
+
+    .authFormWrapper {
+        width: 450px;
+        /*min-height: 550px;*/
+        background-color: white;
+        border-radius: 15px;
+        padding-bottom: 20px;
+        box-shadow:0 0 5px 0 rgba(0,0,0,0.2);
+    }
+
+    .authFormWrapper-switcher {
+        border-bottom:3px solid #eee;
+        min-height: 50px;
+        margin: 30px 0 0 0;
+        display: flex;
+        align-items: stretch;
+    }
+
+    .switcher {
+        margin-right: 15px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 10px;
+        color: #aaa;
+    }
+
+    .switcher--active {
+        border-bottom:3px solid #9038CC;
+        color: #9038CC;
+    }
+
+</style>

66392daba43b250a7b28219b96121f68bb787a49
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-04-18 22:34:46 +0430

    Sentry added.

--- src/main.js
+++ src/main.js
@@ -8,7 +8,13 @@ import Notifications from 'vue-notification'
 import VueLoading from 'vuejs-loading-plugin';
 import VueWindowSize from 'vue-window-size';
 import VModal from 'vue-js-modal'
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
 
+Sentry.init({
+    dsn: 'https://cf45074bfa2d4151acd120f061285fbe@o379552.ingest.sentry.io/5204566',
+    integrations: [new VueIntegration({Vue, attachProps: true})],
+});
 Vue.config.productionTip = false;
 Vue.use(Vuelidate);
 Vue.use(Notifications);

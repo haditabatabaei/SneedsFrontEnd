e96a29ca0ee4f5bf9551424b58a953513126d4d2
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-04-30 22:46:04 +0430

    Some auto rate stuff fixed.

--- src/layouts/ConsultantListLayout.vue
+++ src/layouts/ConsultantListLayout.vue
@@ -11,7 +11,7 @@
                         </ul>
                         <button class="topFilter-sort-button isansFont" @click="toggleRateSort">
                             بیشترین امتیاز
-                            <i class="material-icons" v-if="orderRateAscending">keyboard_arrow_down</i>
+                            <i class="material-icons" v-if="orderRateDescending">keyboard_arrow_down</i>
                             <i class="material-icons" v-else>keyboard_arrow_up</i>
                         </button>
                     </div>

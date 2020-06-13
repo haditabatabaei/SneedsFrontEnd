1f24b7cd53ae198c0a7f9f3193820ddf0f6d8f73
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-06-13 17:37:21 +0430

    Landing sneeds awards added.

--- src/views/Package/Landing.vue
+++ src/views/Package/Landing.vue
@@ -623,6 +623,43 @@
                 </p>
             </div>
         </section>
+        <section class="package-section package-section-awards">
+            <p class="awards-sup isansFont--faNum">
+                در 2 سال گذشته
+            </p>
+            <h1 class="awards-title isansFont">
+                دستاورد های اسنیدز
+            </h1>
+            <div class="section-sub section-sub-awards">
+                <div class="awards-item">
+                    <img alt="2000 پذیرش اخذ شده توسط مشاوران اسنیدز" src="/sneedsAssets/img/awards-login.svg" class="awards-item-icon">
+                    <p class="isansFont--faNum awards-item-amount">56+</p>
+                    <p class="isansFont awards-item-desc">
+                        پذیرش اخذ شده
+                        <br>
+                        به دست مشاوران اسنیدز
+                    </p>
+                </div>
+                <div class="awards-item">
+                    <img alt="2000 پذیرش اخذ شده توسط مشاوران اسنیدز" src="/sneedsAssets/img/awards-people.svg" class="awards-item-icon">
+                    <p class="isansFont--faNum awards-item-amount">27,000+</p>
+                    <p class="isansFont awards-item-desc">
+                        دقیقه مشاوره آنلاین
+                        <br>
+                        انجام شده!
+                    </p>
+                </div>
+                <div class="awards-item">
+                    <img alt="2000 پذیرش اخذ شده توسط مشاوران اسنیدز" src="/sneedsAssets/img/awards-docs.svg" class="awards-item-icon">
+                    <p class="isansFont--faNum awards-item-amount">540+</p>
+                    <p class="isansFont awards-item-desc">
+                        مدارک ویرایش یا نگارش شده
+                        <br>
+                        به دست مشاوران اسنیدز
+                    </p>
+                </div>
+            </div>
+        </section>
         <section class="package-section package-section-faq">
             <div class="section-sub section-sub-faq">
                 <section class="faq-box" v-for="faqBox in faqBoxes">
@@ -1722,6 +1759,70 @@
         line-height: 26px;
     }
 
+    .package-section-awards {
+        background-color: #8C3DDB;
+        min-height: 250px;
+        height: 300px;
+        position: relative;
+        display: flex;
+        margin-bottom: 175px;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    .section-sub-awards {
+        background-color: white;
+        height: 250px;
+        position: absolute;
+        bottom: -100px;
+        border-radius: 10px;
+        box-shadow: 0 4px 16px #00000021;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    .awards-title {
+        color: white;
+        font-weight: bold;
+        font-size: 24px;
+        margin: 10px;
+    }
+
+    .awards-sup {
+        background-color: white;
+        color: #8C3DDB;
+        border-radius: 20px;
+        padding: 5px 15px;
+        margin-top: 30px;
+    }
+
+    .awards-item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .awards-item-icon {
+        width: 40px;
+        height: 40px;
+    }
+
+    .awards-item-amount {
+        color: #303143;
+        font-weight: bold;
+        margin:10px 0;
+        font-size: 16px;
+    }
+
+    .awards-item-desc {
+        text-align: center;
+        color: #9B9999;
+        font-size: 13px;
+    }
+
     @media only screen and (max-width: 991.8px) {
         .section-intro-aparat {
             width: 90%;
@@ -1827,6 +1928,10 @@
 
         .faq-question {
             padding: 10px 0;
+        }
+
+        .section-sub-college {
+            margin: 10px 0;
         }
     }
 </style>

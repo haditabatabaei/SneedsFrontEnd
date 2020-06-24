e852da3230636dfc32b1270cb04926f2a4dabacf
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-06-24 16:52:08 +0430

    User profile page updated with password module and detailed info module integrated in one.

--- src/views/User/UserProfile.vue
+++ src/views/User/UserProfile.vue
@@ -1,61 +1,98 @@
 <template>
-        <div class="col-md-12 infoBlock">
-            <form @submit.prevent="updateUser" class="info-form isansFont">
-                <div class="row">
-                    <div class="col-md-6">
-                        <label for="email" class="info-label isansFont--faNum">ایمیل (غیر قابل تغییر) :
-                            <input id="email" :placeholder="user.email" type="text" class="info-input" disabled>
-                        </label>
-                    </div>
-                    <div class="col-md-6">
-                        <label for="phoneNumber" class="info-label">
-                            شماره تماس (غیر قابل تغییر) :
-                            <input type="text" :placeholder="user.phone_number" id="phoneNumber" class="info-input" disabled>
-                        </label>
-                    </div>
-
-                    <div class="col-md-6">
-                        <label for="firstName" class="info-label">
-                            نام :
-                            <input type="text" :placeholder="firstNamePlaceholder" v-model.trim="inputUser.first_name" id="firstName" class="info-input">
-                            <span class="text-danger isansFont--faNum" v-if="firstNameIsInvalid">نام وارد شده معتبر نیست.</span>
-                        </label>
-                    </div>
-
-                    <div class="col-md-6">
-                        <label for="lastName" class="info-label">
-                            نام خانوادگی :
-                            <input type="text" :placeholder="lastNamePlaceholder" v-model.trim="inputUser.last_name" id="lastName" class="info-input">
-                            <span class="text-danger isansFont--faNum" v-if="lastNameIsInvalid">نام خانوادگی وارد شده معتبر نیست.</span>
-                        </label>
-                    </div>
-
-                    <div class="col-md-6 col-md-offset-6 text-left mt-10">
-                        <button class="btn btn-success isansFont--faNum">ثبت تغییرات</button>
-                    </div>
-                </div>
+        <section class="profile-wrapper">
+            <form @submit.prevent="updateUser" class="profile-form isansFont--faNum">
+                <label for="email" class="profile-label">
+                    <span class="profile-label-name">ایمیل (غیر قابل تغییر)</span>
+                    <input id="email" :placeholder="user.email" type="text" class="profile-input" disabled>
+                </label>
+                <label for="phoneNumber" class="profile-label">
+                    <span class="profile-label-name">شماره تماس (غیر قابل تغییر)</span>
+                    <input id="phoneNumber" :placeholder="user.phone_number" type="text" class="profile-input" disabled>
+                </label>
+                <label for="firstName" class="profile-label">
+                    <span class="profile-label-name">نام:</span>
+                    <input id="firstName" :placeholder="user.first_name" type="text" class="profile-input" v-model="inputUser.first_name">
+                    <span class="profile-label-error" v-if="firstNameIsInvalid">
+                        لطفا یک نام معتبر وارد کنید.
+                    </span>
+                </label>
+                <label for="lastName" class="profile-label">
+                    <span class="profile-label-name">نام خانوادگی:</span>
+                    <input id="lastName" :placeholder="user.last_name" type="text" class="profile-input" v-model="inputUser.last_name">
+                    <span class="profile-label-error" v-if="lastNameIsInvalid">
+                        لطفا یک نام خانوادگی معتبر وارد کنید.
+                    </span>
+                </label>
+                <button class="profile-submit">ثبت تغییرات</button>
             </form>
-        </div>
+            <div class="profile-password isansFont">
+                <button class="profile-password-toggler" @click="isShowingPasswordChange = !isShowingPasswordChange">
+                    <span v-if="!isShowingPasswordChange">میخواهم رمز عبور خود را تغییر دهم</span>
+                    <span v-else>مخفی کردن تغییر رمز عبور</span>
+                    <i class="material-icons" v-if="!isShowingPasswordChange">keyboard_arrow_left</i>
+                    <i class="material-icons" v-else>keyboard_arrow_up</i>
+                </button>
+                <form v-if="isShowingPasswordChange" class="profile-form" @submit.prevent="changePassword">
+                    <label for="password" class="profile-label">
+                        <span class="profile-label-name">رمز عبور:</span>
+                        <input id="password" placeholder="رمز عبور" type="password" class="profile-input" v-model="auth.password">
+                        <span class="profile-label-error" v-if="passwordIsInvalid">
+                        لطفا رمز عبور معتبر وارد کنید.
+                    </span>
+                    </label>
+                    <label for="confirmPassword" class="profile-label">
+                        <span class="profile-label-name">تکرار رمز عبور:</span>
+                        <input id="confirmPassword" placeholder="تکرار رمز عبور" type="password" class="profile-input" v-model="auth.password2">
+                        <span class="profile-label-error" v-if="confirmPasswordIsInvalid">
+                        لطفا تکرار رمز عبور را معتبر وارد کنید.
+                    </span>
+                    </label>
+                    <button class="profile-submit">ویرایش رمز عبور</button>
+                </form>
+            </div>
+            <button v-if="!isConsultant" class="profile-password-toggler isansFont" style="margin-top: 20px;" @click="isShowingCompleteForm = !isShowingCompleteForm">
+                <span v-if="!isShowingCompleteForm">مشاهده اطلاعات تکمیلی</span>
+                <span v-else>مخفی کردن اطلاعات تکمیلی</span>
+                <i class="material-icons" v-if="!isShowingCompleteForm">keyboard_arrow_left</i>
+                <i class="material-icons" v-else>keyboard_arrow_up</i>
+            </button>
+            <package-form v-if="isShowingCompleteForm && !isConsultant" />
+        </section>
 </template>
 
 <script>
-    import axios from 'axios'
+    import PackageForm from "@/components/Packages/PackageForm";
+    import {required, minLength, maxLength, sameAs} from 'vuelidate/lib/validators'
     export default {
         name: "UserProfile",
+        components: {
+            'package-form': PackageForm
+        },
         validations :{
             inputUser : {
-                first_name : {  },
-                last_name : {  },
-            }
+                first_name : {required, minLength: minLength(1), maxLength: maxLength(100)},
+                last_name : {required, minLength: minLength(1), maxLength: maxLength(100)},
+            },
+            auth: {
+                password: {required, minLength: minLength(6), maxLength: maxLength(100)},
+                password2: {required, samAs: sameAs('password')}
+            },
         },
 
         data() {
             return {
-                submitted : false,
+                profileSubmitted : false,
+                passwordSubmitted: false,
+                isShowingPasswordChange: false,
+                isShowingCompleteForm: false,
                 inputUser : {
                     first_name : '',
                     last_name : '',
                 },
+                auth: {
+                    password: '',
+                    password2: ''
+                }
             }
         },
 
@@ -64,65 +101,87 @@
         },
 
         methods : {
+            async changePassword() {
+                this.$v.auth.$touch();
+                this.passwordSubmitted = true;
+                if(!this.passwordFormIsInvalid) {
+                    //password form is valid continue
+                    let payload = {
+                        "password": this.auth.password,
+                        "password2": this.auth.password2
+                    };
+                    try {
+                        let editResult = await this.$api.put(`${this.api}/auth/accounts/${this.userInfo.id}/`, payload, this.httpConfig);
+                        console.log(editResult);
+                        this.auth.password = '';
+                        this.auth.password2 = '';
+                        this.$notify({
+                            group: 'notif',
+                            type: 'success',
+                            title: 'رمز عبور: موفق',
+                            text: 'رمز عبور شما با موفقیت ویرایش شد.'
+                        })
+                    } catch (e) {
+                        this.$notify({
+                            group: 'notif',
+                            type: 'error',
+                            title: 'رمز عبور: خطا',
+                            text: 'خطایی هنگام ویرایش رمز عبور شما رخ داد.'
+                        })
+                    }
+                } else {
+                    //password form is invalid show error
+                    this.$notify({
+                        group: 'notif',
+                        type: 'warn',
+                        title: 'رمز عبور: اخطار',
+                        text: 'لطفاً ورودی های رمز عبور خود را کنترل کنید.'
+                    })
+                }
+            },
+
+
             async updateUser() {
                 this.$v.inputUser.$touch();
-                this.submitted = true;
+                this.profileSubmitted = true;
                 console.log('update user called ', this.inputUser , 'is invalid : ', this.formIsInvalid);
-                if(!this.formIsInvalid) {
+                if(!this.profileFormIsInvalid) {
+                    let payload = {
+                        "first_name": this.inputUser.first_name,
+                        "last_name": this.inputUser.last_name,
+                    };
                     try {
-                        console.log('form is good dispatching requests.')
-                        //this.$loading(true);
-                        let editResult = await this.$api.put(`${this.$store.getters.getApi}/auth/accounts/${this.$store.getters.getUserInfo.id}/`,this.generatePayload(),this.$store.getters.httpConfig);
+                        let editResult = await this.$api.put(`${this.api}/auth/accounts/${this.userInfo.id}/`, payload, this.httpConfig);
                         console.log(editResult);
                         await this.$store.dispatch('getUserWithId', this.$store.getters.getUserInfo.id);
-                        this.printMessage("اطلاعات شما با موفقیت به روز رسانی شد.", "ویرایش : موفق", "success", "4000", "notif")
+                        this.$notify({
+                            group: 'notif',
+                            type: 'success',
+                            title: 'اطلاعات کاربری: موفق',
+                            text: 'اطلاعات کاربری شما با موفقیت ویرایش شد.'
+                        })
                     } catch (e) {
                         console.log(e);
                         if(e.response) {
                             console.log(e.response);
-                            if(e.response.detail) {
-                                this.printMessage(e.response.data.detail, "ویرایش : خطا", "error", 3000, "notif");
-                            } else {
-                                this.printMessage( this.stringifyError(e.response.data), "ویرایش :‌ خطا", "error", 3000, "notif");
-                            }
                         }
-                    } finally {
-
+                        this.$notify({
+                            group: 'notif',
+                            type: 'error',
+                            title: 'اطلاعات کاربری: خطا',
+                            text: 'خطایی هنگام ویرایش اطلاعات کاربری شما رخ داد.'
+                        })
                     }
                 } else {
-                    this.printMessage("لطفا اطلاعات ورودی خود را کنترل کنید.", "ویرایش: اخطار", "warn", 3000, "notif");
+                    this.$notify({
+                        group: 'notif',
+                        type: 'warn',
+                        title: 'اطلاعات کاربری: اخطار',
+                        text: 'لطفاً ورودی های نام و نام خانوادگی خود را کنترل کنید.'
+                    })
                 }
             },
 
-            generatePayload() {
-                console.log(this.inputUser);
-                let payload = {};
-                for(let item in this.inputUser) {
-                    if(this.inputUser[item].length !== 0) {
-                        payload[item] = this.inputUser[item];
-                    }
-                }
-                console.log(payload);
-                return payload;
-            },
-
-            printMessage(text, title, type, duration, group) {
-                this.$notify({
-                    group: group,
-                    text: text,
-                    title: title,
-                    type: type,
-                    duration: duration
-                })
-            },
-
-            stringifyError(errorObj) {
-                let string = '<br>';
-                for (let item in errorObj) {
-                    string += errorObj[item].join('<br>') + '<br>';
-                }
-                return string;
-            },
         },
 
         computed : {
@@ -130,46 +189,118 @@
                 return this.$store.getters.getUser;
             },
 
+            userInfo() {
+                return this.$store.getters.getUserInfo;
+            },
+
+            api() {
+                return this.$store.getters.getApi;
+            },
+
+            isConsultant() {
+                return this.$store.getters.isConsultant;
+            },
+
+            httpConfig() {
+                return this.$store.getters.httpConfig;
+            },
+
             firstNameIsInvalid() {
-                return this.inputUser.first_name.length !== 0 && this.submitted && this.$v.inputUser.first_name.$error;
+                return this.profileSubmitted && this.$v.inputUser.first_name.$error;
             },
 
             lastNameIsInvalid() {
-                return this.inputUser.last_name.length !== 0 && this.submitted && this.$v.inputUser.last_name.$error;
+                return this.profileSubmitted && this.$v.inputUser.last_name.$error;
             },
 
-            formIsInvalid() {
-                return this.submitted && (this.firstNameIsInvalid || this.lastNameIsInvalid);
+            passwordIsInvalid() {
+                return this.passwordSubmitted && this.$v.auth.password.$error;
             },
-            firstNamePlaceholder() {
-                if(this.user.first_name == null) {
-                    return 'ثبت نشده'
-                } else {
-                    return this.user.first_name
-                }
+
+            confirmPasswordIsInvalid() {
+                return this.passwordSubmitted && this.$v.auth.password2.$error;
             },
-            lastNamePlaceholder() {
-                if(this.user.last_name == null) {
-                    return 'ثبت نشده'
-                } else {
-                    return this.user.last_name
-                }
-            }
+
+            profileFormIsInvalid() {
+                return this.profileSubmitted && (this.firstNameIsInvalid || this.lastNameIsInvalid);
+            },
+
+            passwordFormIsInvalid() {
+                return this.passwordSubmitted && (this.passwordIsInvalid || this.confirmPasswordIsInvalid);
+            },
         }
     }
 </script>
 
 <style scoped>
-    .infoBlock {
+
+    .profile-wrapper {
         background-color: white;
         border-radius: 15px;
         box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
         display: flex;
-        align-items: flex-start;
+        align-items: stretch;
         justify-content: flex-start;
         flex-direction: column;
         margin-top: 30px;
         padding-bottom: 15px;
+    }
+
+    .profile-form {
+        margin: 15px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+    }
+
+    .profile-label {
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+        margin: 5px;
+        width: calc(50% - 20px);
+    }
+
+    .profile-input {
+        margin-top:5px;
+        padding:0 5px;
+        height: 40px;
+        border-radius: 10px;
+        border:1px solid #ccc;
+        color: #222;
+    }
+
+    .profile-submit {
+        border: none;
+        background-color: #3CAEA3;
+        color: white;
+        border-radius: 10px;
+        padding: 7.5px 30px;
+        margin:5px auto 5px 10px;
+    }
+
+    .profile-submit:hover {
+        background-color: #3ba499;
+    }
+
+    .profile-label-error {
+        font-size: 12px;
+        color: #c9737c;
+        margin-top: 5px;
+    }
+
+    .profile-password-toggler {
+        background: none;
+        border: none;
+        margin-right: 20px;
+        display: flex;
+        align-items: center;
+        font-size: 13px;
+        color: #585858;
+    }
+
+    .profile-password-toggler:hover {
+        color: #8C3DDB;
     }
 
     .info-form {

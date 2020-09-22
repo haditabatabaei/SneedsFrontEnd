e7fc97c84b0924fd0b616ccd2fe2ff0b40235789
Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-09-22 20:09:38 +0330

    Form result basic components added.

---
+++ src/components/FormResult/ResultTab.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="tabs gadugiFont">
+        <div class="tabs-selectors">
+            <button class="tabs-selector" :class="[{'tabs-selector--active': activeTab == tab}]" v-for="tab in tabs" @click="activeTab = tab">{{tab.title}}</button>
+        </div>
+        <div class="tabs-content">
+            <slot v-for="(tab, index) in tabs" :name="`TAB${index}`" v-if="activeTab == tab"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ResultTab",
+        data() {
+            return {
+                activeTab: null
+            }
+        },
+        props: {
+            tabs: {
+                type: Array,
+            }
+        },
+        methods: {},
+        created() {
+            this.activeTab = this.tabs[0];
+        }
+    }
+</script>
+
+<style scoped>
+    .tabs {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .tabs-selectors {
+        display: flex;
+        border-bottom: 5px solid #ccc;
+    }
+
+    .tabs-selector {
+        background: none;
+        padding: 10px 15px;
+        border: none;
+        font-weight: bold;
+        color: #374957;
+        position: relative;
+    }
+
+    .tabs-selector--active {
+        color: #009FB3;
+    }
+
+    .tabs-selector--active::after {
+        content: " ";
+        width: 100%;
+        position: absolute;
+        height: 5px;
+        background-color: #009FB3;
+        bottom: -5px;
+    }
+
+    .tabs-content {
+        margin: 10px;
+        color: #707070;
+    }
+</style>
---
+++ src/components/FormResult/ScoreOutOfMax.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="scoreofmax gadugiFont">
+        <p class="score-value">{{scoreValue}}</p>
+        <p class="score-max">/{{scoreMax}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ScoreOutOfMax",
+        data() {
+            return {}
+        },
+        props: {
+            scoreValue: {
+                type: Number
+            },
+            scoreMax: {
+                type: Number
+            }
+        },
+        methods: {},
+        created() {
+
+        }
+    }
+</script>
+
+<style scoped>
+    .scoreofmax {
+        display: flex;
+    }
+
+    .score-value {
+        color: #173F5F;
+        font-size: 32px;
+        font-weight: 900;
+    }
+
+    .score-max {
+        color: #007994;
+        font-size: 18px;
+        font-weight: 900;
+        margin-left: 2px;
+        align-self: flex-end;
+        margin-bottom: 5px;
+    }
+</style>

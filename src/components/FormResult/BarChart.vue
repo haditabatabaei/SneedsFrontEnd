0761634513d36aaf20ebce78430d48ac4687d49a
Seyed Mohammad Hadi Tabatabaei <hadi.tabatabaei.aut@gmail.com>
2020-09-25 20:02:21 +0330

    Bar chart reactivity added.

--- src/components/FormResult/BarChart.vue
+++ src/components/FormResult/BarChart.vue
@@ -1,26 +1,19 @@
 <script>
-    import { Bar } from 'vue-chartjs'
+    import { Bar, mixins } from 'vue-chartjs'
     export default {
         name: "BarChart",
         extends: Bar,
-        data() {
-            return {
-
-            }
-        },
-        props: {
-            data: {
-                type: Object,
-                default: () => null
-            },
-            options: {
-                type: Object,
-                default: () => null
+        mixins: [mixins.reactiveProp],
+        props: ['chartData', 'options'],
+        watch: {
+            options(newVal) {
+                this._chart.destroy();
+                this.renderChart(this.chartData, newVal);
             }
         },
         methods: {},
         mounted() {
-            this.renderChart(this.data, this.options);
+            this.renderChart(this.chartData, this.options);
         }
     }
 </script>

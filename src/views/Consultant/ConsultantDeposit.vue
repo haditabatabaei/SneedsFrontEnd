e02b1bc88a19456c2251b42e01f84573e4cb98ed
Sahar Arefzadeh <SaharArefzadeh@Sahar-Arefzadeh.local>
2020-10-03 17:52:29 +0330

    Deposit table edited and deposit wrapper added.

--- src/views/Consultant/ConsultantDeposit.vue
+++ src/views/Consultant/ConsultantDeposit.vue
@@ -1,50 +1,53 @@
 <template>
-  <div>
-    <h2 class="deposit-title">
-      Deposit
-    </h2>
+    <section class="deposit-wrapper gadugiFont">
+      <h2 class="deposit-title">
+        Deposit
+      </h2>
 
-    <div class="grid-table-responsive">
-      <table class="grid-table">
-        <thead class="grid-head">
-        <tr class="grid-row">
-          <th class="grid-head-cell">Row</th>
-          <th class="grid-head-cell">Time</th>
-          <th class="grid-head-cell cell--centered">
-           ID
-          </th>
-          <th class="grid-head-cell cell--centered">
-            Amount
-          </th>
-          <th class="grid-head-cell cell--centered">
-            Comment
-          </th>
-        </tr>
-        </thead>
-        <tbody class="grid-body">
-        <tr class="grid-row" v-for="(payment, index) in payments" :key="index">
-          <td class="grid-cell">{{index+1}}</td>
-          <td class="grid-cell">{{getJalali(payment.updated).format('	YYYY, MMM, Do, h:mm:ss a')}}</td>
-          <td class="grid-cell cell--centered">
-            <p>
-              {{payment.consultant_deposit_info_id}}
-            </p>
-          </td>
-          <td class="grid-cell cell--centered">
-            <p>
-              {{payment.amount}}
-            </p>
-          </td>
-          <td class="grid-cell cell--centered">
-            <p>
-              {{payment.comment}}
-            </p>
-          </td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
+      <div class="grid-table-responsive">
+        <table class="grid-table">
+          <thead class="grid-head">
+          <tr class="grid-row">
+            <th class="grid-head-cell">Time</th>
+            <th class="grid-head-cell cell--centered">
+             ID
+            </th>
+            <th class="grid-head-cell cell--centered">
+              Amount
+            </th>
+            <th class="grid-head-cell cell--centered">
+              Comment
+            </th>
+          </tr>
+          </thead>
+          <tbody class="grid-body">
+          <tr class="grid-row" v-for="(payment, index) in payments" :key="index">
+            <td class="grid-cell">
+              {{getJalali(payment.updated).format('	YYYY, MMM, Do, h:mm:ss a')}}
+            </td>
+            <td class="grid-cell cell--centered">
+              <p>
+                {{payment.consultant_deposit_info_id}}
+              </p>
+            </td>
+            <td class="grid-cell cell--centered">
+              <p>
+                {{payment.amount}}
+              </p>
+            </td>
+            <td class="grid-cell cell--centered">
+              <p>
+                {{payment.comment}}
+              </p>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <p v-if="payments.length === 0" class="no-record-message">
+        No records found.
+      </p>
+    </section>
 </template>
 <script>
 import jalali from 'jalali-moment';
@@ -89,18 +92,30 @@ export default {
 }
 </script>
 <style scoped>
-
+.deposit-wrapper {
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: column;
+  margin-top: 30px;
+  padding-bottom: 15px;
+}
 .deposit-title {
   color: #374957;
   font-size: 26px;
   margin: 50px 0 10px 10px;
   font-weight: normal;
 }
-
+ .grid-table-responsive {
+   width: 100%;
+ }
 .grid-table {
   display: grid;
   border-collapse: collapse;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(4, 1fr);
 }
 
 .grid-head,
@@ -150,9 +165,16 @@ export default {
   justify-content: center;
 }
 
+.no-record-message {
+  color: #808080;
+  align-self: center;
+}
 @media only screen and (max-width: 767.8px) {
+  .deposit-wrapper {
+    overflow: auto;
+  }
   .grid-table {
-    grid-template-columns: repeat(5, 150px);
+    grid-template-columns: repeat(4, 185px);
     overflow: auto;
   }
 
